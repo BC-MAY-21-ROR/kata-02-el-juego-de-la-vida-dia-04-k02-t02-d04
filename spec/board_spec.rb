cd51572176a114c97e652ed17d3385717c705769
julio cabrera <juliocabrera820@gmail.com>
2021-06-16 03:47:10 -0500

    write context

---
+++ spec/board_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require_relative '../src/board'
+
+describe Board do
+  xdescribe '#next_generation' do
+    context 'first generation contains 3 alive cells' do
+    end
+  end
+end
